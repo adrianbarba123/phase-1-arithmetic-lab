d0bdd1e9a3b29ccee7cc66494767cdb22afdc8a7
Adrian Barba <adrianbarba123@gmail.com>
2022-11-02 08:46:06 -0700

    commit tree 842c1edf6b9f703a8e8e22ffc847848807dec681

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+let num1 = 31, num2 = 2;
+let multiply = num1 * num2;
 
-// Write your code here
+let random = Math.floor(Math.random()) + 1;
+
+let num3 = 4;
+let num4 = 5;
+let mod = num3 % num4;
+
+let max;
+max = Math.max(20);
+
+const isAdult = age >= 18;
+console.log(isAdult);
